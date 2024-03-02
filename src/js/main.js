f05e49bf18afef52b09b4a7f52875ac27d170a69
Norman Bui <normanbui23@gmail.com>
2024-03-02 10:47:25 -0500

    initial frontend

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for the Sign Up button
+    document.getElementById('signupBtn').addEventListener('click', function() {
+        window.location.href = 'signup.html'; // Change to your signup page's URL
+    });
+
+    // Event listener for the Login button
+    document.getElementById('loginBtn').addEventListener('click', function() {
+        window.location.href = 'login.html'; // Change to your login page's URL
+    });
+});
